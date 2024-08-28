94a06526919b69520e9bddb43f2a1aee5476f7d7
kimkihyune <kimkihyunee123@gmail.com>
2024-08-28 11:33:53 +0900

    Add files via upload

---
+++ pycoding01/learning01.py
@@ -0,0 +1,15 @@
+
+print('Hellow world!!')
+
+print(10+20)
+print('10'+'20')
+
+num1=input('첫번째 숫자 입력')
+num2=inupt('두번째 숫자 입력')
+print('두 수의 합:',num1+num2)
+
+# 나이 입력:17
+#당신의 나이는 17입니다
+
+year = int(input(' 생년 입력:'))
+print(' 당신의 나이는 ',2024-year+1,'입니다')
